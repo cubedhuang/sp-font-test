2d2960bcec5bf7cedd865f6be796229789da2fbe
cubedhuang <99danielh@gmail.com>
2024-02-15 18:32:04 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const removedWords = [
+		'anu',
+		'e',
+		'en',
+		'la',
+		'li',
+		'pi',
+		'kiki',
+		'linluwi',
+		'oke',
+		'omekapo',
+		'nimisin',
+		'usawi',
+		'wuwojiti',
+		'yupekosi'
+	];
+
+	let before = '';
+	let separator = '+';
+	let after = '';
+
+	let rawWords = '';
+
+	$: words = rawWords.split('\n');
+
+	onMount(async () => {
+		const data = (await fetch(
+			'https://raw.githubusercontent.com/lipu-linku/jasima/main/data.json'
+		).then(res => res.json())) as {
+			data: Record<
+				string,
+				{
+					word: string;
+					usage_category: string;
+					book: string;
+				}
+			>;
+		};
+
+		rawWords = Object.values(data.data)
+			.filter(
+				word =>
+					word.usage_category === 'core' ||
+					word.usage_category === 'widespread' ||
+					word.usage_category === 'common' ||
+					word.usage_category === 'uncommon' ||
+					word.book === 'ku suli'
+			)
+			.filter(word => !removedWords.includes(word.word))
+			.map(word => word.word)
+			.join('\n');
+	});
+
+	let constantWord = 'ijo';
+	let varyFirstWord = false;
+
+	$: output = words.map(word => {
+		if (varyFirstWord) {
+			return `${before}${word}${separator}${constantWord}${after}`;
+		} else {
+			return `${before}${constantWord}${separator}${word}${after}`;
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>nasin nanpa combo playground</title>
+</svelte:head>
+
+<div class="mt-20 max-w-screen-lg mx-auto px-8">
+	<h1 class="font-bold text-4xl">nasin nanpa combo playground</h1>
+
+	<div class="mt-4 flex items-baseline">
+		<input
+			type="text"
+			bind:value={before}
+			class="input px-2 py-1 w-12 text-center"
+		/>
+
+		<p class="mx-2">first word</p>
+
+		<input
+			type="text"
+			bind:value={separator}
+			class="input px-2 py-1 w-12 text-center"
+		/>
+
+		<p class="mx-2">second word</p>
+
+		<input
+			type="text"
+			bind:value={after}
+			class="input px-2 py-1 w-12 text-center"
+		/>
+	</div>
+
+	<textarea class="mt-4 input h-32 w-full p-4" bind:value={rawWords}
+	></textarea>
+
+	<div class="mt-4 flex">
+		<select
+			class="input px-4 py-1 cursor-pointer"
+			bind:value={constantWord}
+		>
+			{#each words as word}
+				<option value={word}>{word}</option>
+			{/each}
+		</select>
+
+		<label class="ml-4">
+			<input
+				type="checkbox"
+				bind:checked={varyFirstWord}
+				class="cursor-pointer"
+			/>
+			<span class="ml-2">vary first word</span>
+		</label>
+	</div>
+
+	<div class="mt-4 flex flex-wrap">
+		{#each output as combo}
+			<p class="border font-pona p-2 text-6xl">
+				{combo}
+			</p>
+		{/each}
+	</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+				pona: ['"nasin-nanpa"', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
+	plugins: [
+		plugin(function ({ addVariant }) {
+			addVariant('hf', ['&:hover', '&:focus']);
+			addVariant('hv', ['&:hover', '&:focus-visible']);
+			addVariant('group-hf', ['.group:hover &', '.group:focus &']);
+			addVariant('group-hv', [
+				'.group:hover &',
+				'.group:focus-visible &'
+			]);
+		})
+	]
+};
